6638d5290cfdbfe76e86c0578c8686454149000f
Lixiang Ao <aolixiang@gmail.com>
2020-02-21 14:00:01 -0800

    test runs

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ import workload
 def main():
     # create cluster
     hosts = []
-    for i in range(5):
+    for i in range(3):
         hosts.append(Host(i, 10))
     cluster = Cluster(hosts)
 
@@ -15,15 +15,19 @@ def main():
     functions = []
     for i in range(1):
         functions.append(Function(1, 2))
+        functions.append(Function(2, 2))
+        functions.append(Function(3, 2))
+        functions.append(Function(4, 2))
+        
 
     # define invocations
-    invocations = {3:[]}
-    for _ in range(26):
-        invocations[3].append(Invocation(functions[0], 4))
+    invocations = {1:[]}
+    for n in range(12):
+        invocations[1].append(Invocation(functions[n%4], 4))
 
     invoke_queue = []
     # ticks
-    for e in range(15):
+    for e in range(10):
         cluster.epoch = e
         cluster.tick()
         for i in invocations.get(cluster.epoch, []):
@@ -47,24 +51,27 @@ def main():
 def main2():
     # create cluster
     hosts = []
-    for i in range(10):
+    for i in range(50):
         hosts.append(Host(i, 4))
     cluster = Cluster(hosts)
 
     # workloads
-    requests1 = workload.burst_parallel(100)
-    requests2 = workload.burst_parallel(100)
+    requests1 = workload.burst_parallel(22)
+    requests2 = workload.burst_parallel(25)
+    requests3 = workload.burst_parallel(18)
 
     # ticks
     invoke_queue = []
-    for e in range(40):
+    for e in range(50):
         cluster.epoch = e
         cluster.tick()
         for i in requests1.get(cluster.epoch-2, []):
             invoke_queue.append(i)
         for i in requests2.get(cluster.epoch-4, []):
             invoke_queue.append(i)
-        
+        for i in requests3.get(cluster.epoch-5, []):
+            invoke_queue.append(i)
+
         print('scheduling...')
         i = 0
         while i < len(invoke_queue):
