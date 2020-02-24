d616f7b6412f883e0087b8d5743a264bbbcec256
Lixiang Ao <aolixiang@gmail.com>
2020-02-23 17:00:53 -0800

    update run.py

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
+import random
 from functools import reduce
 import workloads
-from cluster import Cluster, Host, Function, Invocation, dashboard
+from cluster import Cluster, Host, Function, Invocation
 
 def main():
     # create cluster
@@ -15,46 +16,51 @@ def main():
     functions = []
     for i in range(1):
         functions.append(Function(1, 2))
-        # functions.append(Function(2, 2))
+        functions.append(Function(3, 2))
         # functions.append(Function(3, 2))
         # functions.append(Function(4, 2))
         
     # define invocations
     invocations = {0:[]}
-    for n in range(12):
+    for n in range(16):
         invocations[0].append(Invocation(functions[n%len(functions)], 4))
 
     # ticks
-    for _ in range(10):
+    while len(invocations) > 0 or not c.is_idle():
         for i in invocations.get(c.epoch, []):
             c.request(i)
+        invocations.pop(c.epoch, None)
+
         c.tick()
         c.describe()
-    dashboard()
+    c.dashboard()
 
-def main2():
+def main3():
     # create cluster
     hosts = []
-    for i in range(10):
+    for i in range(100):
         hosts.append(Host(i, 4))
     cluster = Cluster(hosts)
 
     # workloads
-    requests = [workloads.burst_parallel(0, 22), 
-                workloads.burst_parallel(3, 25), 
-                workloads.burst_parallel(4, 18), 
-                workloads.burst_parallel(6, 16)]
-    merged = reduce(workloads.merge_invocs, requests)
+    gen = random.Random(123)
+    loads = []
+    for i in range(20): # 20 apps
+        func = Function(gen.randint(0, 2**31), 1)
+        loads.append(workloads.burst_parallel_app(func, 100, start=gen.randint(0, 300)))
+    merged = reduce(workloads.merge_invocs, loads)
 
     # ticks
-    for _ in range(20):
+    while len(merged) > 0 or not cluster.is_idle():
         for i in merged.get(cluster.epoch, []):
             cluster.request(i)
+        merged.pop(cluster.epoch, None)
+
         cluster.tick()
         cluster.describe()
 
-    dashboard()
+    cluster.dashboard()
 
 
 if __name__=='__main__':
-    main2()
+    main3()
