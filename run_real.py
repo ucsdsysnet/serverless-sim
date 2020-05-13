24a10bc24bd5e777599e6afa91f83d8b0f7e93df
Lixiang Ao <aolixiang@gmail.com>
2020-05-13 01:21:39 +0000

    Send real requests to Openwhisk.

--- run_real.py
+++ run_real.py
@@ -6,24 +6,44 @@ from functools import reduce
 import json
 import hashlib
 import requests
+import urllib3
+
 
 import common
 from cluster import Cluster, Host, Function, Invocation, logs, metrics
 import workload
 import plot
 
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 session = requests.Session()
 adapter = requests.adapters.HTTPAdapter(
     pool_connections=100,
     pool_maxsize=100)
 session.mount('http://', adapter)
+session.verify = False
+
+APIHOST = None
+
+assigned_count = [0] * 10
+function_pool = {}
+
+def get_func(invocation):
+    return function_pool[invocation.function.function_id]
 
 def create_function(fnid, demand):
-    pass
+    assigned_count[demand] += 1
+    function_pool[fnid] = 'ram_walk_%d_%d' % (demand * 128, assigned_count[demand])
 
 def request(invocation):
     print('requesting:', invocation, time.time(), file=sys.stderr)
-    session.get("https://www.google.com/")
+    fn = get_func(invocation)
+    url = 'https://' + APIHOST + '/api/v1/namespaces/_/actions/' + fn
+    headers = {'Content-Type':'application/json'}
+    jsondata = {"duration":invocation.duration}
+    resp = session.request('post', url, params={'blocking':'false'}, headers=headers, json=jsondata)
+    print('response:', resp.json(), file=sys.stderr)
+    return resp
 
 def main(seed, workloads, *args, **kwargs):
     common.init_gen(seed)
@@ -37,14 +57,13 @@ def main(seed, workloads, *args, **kwargs):
         all_functions.update(fns)
 
     for f in all_functions:
-        create_function(f.function_id, f.demand)
+        create_function(f.function_id, int(round(f.demand)))
 
     start = time.time() + 5
-
+    print('waiting 5 seconds', file=sys.stderr)
+#    wklds = {3:[Invocation(list(all_functions)[0], 2)], 13:[Invocation(list(all_functions)[0], 1)]}
     def sleep_till(ts):
-        # print('sleep_till: ', ts)
         cur = time.time()
-        # print('current:', cur)
         if ts <= cur:
             print('overflown to next tick!', file=sys.stderr)
             return
@@ -63,6 +82,8 @@ def main(seed, workloads, *args, **kwargs):
     return
 
 if __name__ == '__main__':
+    session.auth = tuple(os.environ['AUTH'].split(':'))
+    APIHOST = os.environ['APIHOST']
     params = json.load(sys.stdin)
     if len(sys.argv) == 2:
         run_id = sys.argv[1]
