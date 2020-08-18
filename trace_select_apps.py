914d7c73c4d6f5fa479b79d42690c64f1ff5983a
Lixiang Ao <liao@eng.ucsd.edu>
2020-08-17 20:41:29 -0700

    Trace analysis

---
+++ trace_select_apps.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import os
+import csv
+import collections
+
+# tracedir = os.environ['AZURE_TRACE_DIR']
+tracedir = '/Users/aolx/develop/azurefunction-dataset'
+infile = 'function_durations_percentiles.anon.d01.csv'
+outfile = 'chosen_apps.csv'
+
+apps = collections.OrderedDict()
+with open(tracedir+'/'+infile, newline='') as fin:
+        reader = csv.reader(fin)
+        reader.__next__()
+        for row in reader:
+            apps[row[1]] = None
+
+with open(outfile, 'w+') as fout:
+    writer = csv.writer(fout)
+    for app in apps.keys():
+        if int(app, 16) % 256 == 1:
+            writer.writerow((app,))
